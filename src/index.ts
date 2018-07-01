cdaf70790897ce283978bda5442532f44dacc6cf
Valentin Cocaud <v.cocaud@gmail.com>
2018-07-01 22:28:41 +0200

    Add gql file extension support

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const read = (schema: string, schemas?: { [key: string]: string }) => {
   return schemas ? schemas[schema] : schema
 }
 
-const isFile = f => f.endsWith('.graphql')
+const isFile = f => f.endsWith('.graphql') || f.endsWith('.gql')
 
 /**
  * Parse a single import line and extract imported types and schema filename
